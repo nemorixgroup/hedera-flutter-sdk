1e9a608c7478f4ab8cb477d0dd0f2aed46e0f86a
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2026-09-17 10:40:42 -0400

    chore: bump version to 0.2.3-dev & updated SDK's barrel file
    Update pubspec.yaml version for fungible token mint/burn support, plus the SDK-wide transaction timestamp reliability fix.

--- lib/hedera_flutter_sdk.dart
+++ lib/hedera_flutter_sdk.dart
@@ -19,6 +19,7 @@ library;
 // Client
 export 'src/client/hedera_client.dart';
 export 'src/client/hedera_network.dart';
+export 'src/client/hedera_node.dart';
 
 // Core
 export 'src/core/hedera_constants.dart';
@@ -51,6 +52,9 @@ export 'src/transactions/account_create_transaction.dart';
 export 'src/transactions/account_delete_transaction.dart';
 export 'src/transactions/account_update_transaction.dart';
 export 'src/transactions/crypto_transfer_transaction.dart';
-export 'src/transactions/token_associate_transaction.dart';
-export 'src/transactions/token_dissociate_transaction.dart';
+export 'src/transactions/tokens/token_associate_transaction.dart';
+export 'src/transactions/tokens/token_burn_transaction.dart';
+export 'src/transactions/tokens/token_create_transaction.dart';
+export 'src/transactions/tokens/token_dissociate_transaction.dart';
+export 'src/transactions/tokens/token_mint_transaction.dart';
 export 'src/transactions/transaction.dart';
